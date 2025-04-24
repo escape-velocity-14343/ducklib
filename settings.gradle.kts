b9549e241d8937ee57cb06d709fb4a12d587042a
Innaias <innaias@harveyosity.org>
2025-04-23 22:00:12 -0700

    Commit in which I fixed things somehow and the build system likes me again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "ducklib"
-include("src:main:test")
-findProject(":src:main:test")?.name = "test"
+
